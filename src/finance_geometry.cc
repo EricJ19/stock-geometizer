d2958b2647089ddb88346befc274b7ccb6975dc7
Eric Jin <ericjin2@illinois.edu>
2020-04-24 10:47:44 -0700

    Create Outline Definitions for finance_geometry.cc

--- src/finance_geometry.cc
+++ src/finance_geometry.cc
@@ -3,3 +3,62 @@
 //
 #include <mylibrary/finance_geometry.h>
 
+void geometry::Geometry::SetOuterColors(double _26_wk_price_return,
+                                        double _3_yr_rev_growth_rate) {
+
+}
+
+void geometry::Geometry::SetOuterEdges(double _26_wk_price_return,
+                                       double _3_yr_rev_growth_rate) {
+
+}
+
+int geometry::Geometry::GetOuterRedColor() {
+
+}
+
+int geometry::Geometry::GetOuterGreenColor() {
+
+}
+
+int geometry::Geometry::GetOuterBlueColor() {
+
+}
+
+int geometry::Geometry::GetOuterEdgeNumber() {
+
+}
+
+
+void geometry::Geometry::SetInnerColors(int buy_rec,
+                                        int sell_rec,
+                                        int hold_rec,
+                                        int strong_buy_rec,
+                                        int strong_sell_rec) {
+
+}
+
+void geometry::Geometry::SetInnerEdges(int buy_rec,
+                                       int sell_rec,
+                                       int hold_rec,
+                                       int strong_buy_rec,
+                                       int strong_sell_rec) {
+
+}
+
+int geometry::Geometry::GetInnerRedColor() {
+
+}
+
+int geometry::Geometry::GetInnerGreenColor() {
+
+}
+
+int geometry::Geometry::GetInnerBlueColor() {
+
+}
+
+int geometry::Geometry::GetInnerEdgeNumber() {
+
+}
+
