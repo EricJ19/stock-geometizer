e183d705c82d23a9be38a826d7b5d1ec97ce43a5
Eric Jin <ericjin2@illinois.edu>
2020-04-29 23:35:31 -0700

    Create Window for User Interaction

--- apps/stock_geometizer.cc
+++ apps/stock_geometizer.cc
@@ -3,13 +3,14 @@
 #include "stock_geometizer.h"
 
 #include <cinder/app/App.h>
+#include <cinder/gl/wrapper.h>
 
+#include "../blocks/Cinder-ImGui/include/CinderImGui.h"
 #include "cpr/api.h"
 #include "cpr/auth.h"
 #include "cpr/cprtypes.h"
 #include "cpr/parameters.h"
 #include "nlohmann/json.hpp"
-#include "../blocks/Cinder-ImGui/include/CinderImGui.h"
 
 namespace myapp {
 
@@ -19,6 +20,7 @@ StockGeo::StockGeo() {
 }
 
 void StockGeo::setup() {
+  ImGui::initialize();
 }
 
 void StockGeo::update() {
@@ -27,7 +29,26 @@ void StockGeo::update() {
 }
 
 void StockGeo::draw() {
+  // Clears the background to black
+  cinder::gl::clear();
 
+  char first_stock_input[4];
+  char second_stock_input[4];
+  char third_stock_input[4];
+
+  ImGui::Begin("Geometizer");
+  cinder::gl::clear( 0);
+  ImGui::Text("Input a Stock");
+
+  ImGui::InputText("Stock1", first_stock_input, sizeof(first_stock_input));
+  ImGui::Button("Ok");
+
+  ImGui::InputText("Stock2", second_stock_input, sizeof(second_stock_input));
+  ImGui::Button("Ok");
+
+  ImGui::InputText("Stock3", third_stock_input, sizeof(third_stock_input));
+  ImGui::Button("Ok");
+  ImGui::End();
 }
 
 void StockGeo::keyDown(KeyEvent event) { }
