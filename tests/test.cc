f03510a35c44fba7f34633de5d2268bbfa9dda19
Eric Jin <ericjin2@illinois.edu>
2020-05-06 13:25:46 -0700

    Create Outline for Tests on Geometry Class

--- tests/test.cc
+++ tests/test.cc
@@ -7,8 +7,18 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Random sanity test", "[random]") {
-  const float random = cinder::randFloat();
-  REQUIRE(0. <= random);
-  REQUIRE(random <= 1.);
+TEST_CASE("Sets outer colors based on stock data",
+    "[outer-color][FinanceData][Geometry]") {
+}
+
+TEST_CASE("Sets inner colors based on stock data",
+    "[inner-color][FinanceData][Geometry]") {
+}
+
+TEST_CASE("Sets outer edges based on stock data",
+    "[outer-edges][FinanceData][Geometry]") {
+}
+
+TEST_CASE("Sets inner edges based on stock data",
+    "[inner-edges][FinanceData][Geometry]") {
 }
